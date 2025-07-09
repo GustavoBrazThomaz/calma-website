ce60b17199e4cbe860c1ebba3d9aa68f6b4e6a60
Gustavo Braz <86533841+GustavoBrazThomaz@users.noreply.github.com>
2025-07-09 19:18:40 -0300

    feat: add appbar to notebook component

--- src/ui/components/Notebook.tsx
+++ src/ui/components/Notebook.tsx
@@ -2,14 +2,23 @@ import Image from "next/image";
 
 export function Notebook() {
   return (
-    <div className="border-2 border-b-0 border-gray-300 rounded-t-[34px] z-10  w-[772px] h-[450px]">
-      <div className="relative w-[768px] h-[450px] border-16 border-b-0 rounded-t-4xl bg-black">
-        <Image
-          layout="fill"
-          src={"/assets/calma-dashboard.png"}
-          alt="calma-dashboard"
-          className="rounded-t-xl pointer-events-none"
-        />
+    <div className="border-2 border-b-0 border-gray-300 rounded-t-[26px] z-10  w-[772px] h-[452px]">
+      <div className="relative w-[768px] h-[450px] border-16 border-b-0 rounded-t-3xl bg-black">
+        <div className="h-6 w-full bg-[#3E3635] rounded-t-xl flex justify-between">
+          <div className="text-sm flex gap-2 p-2 [&_div]:size-2 [&_div]:rounded-full">
+            <div className="bg-red-400" />
+            <div className="bg-amber-400" />
+            <div className="bg-emerald-400" />
+          </div>
+        </div>
+        <div className="relative w-full h-full">
+          <Image
+            layout="fill"
+            src={"/assets/calma-dashboard.png"}
+            alt="calma-dashboard"
+            className=" pointer-events-none object-contain -mt-[12.3px]"
+          />
+        </div>
       </div>
     </div>
   );
